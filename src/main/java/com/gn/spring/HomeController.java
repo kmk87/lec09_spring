6d5b846e604a51f2534200bbe4be827994efeedd
kmk87 <minkyoungk24@gmail.com>
2024-07-31 15:12:43 +0900

    [김민경] 로그 기록 남기기

--- src/main/java/com/gn/spring/HomeController.java
+++ src/main/java/com/gn/spring/HomeController.java
@@ -1,5 +1,7 @@
 package com.gn.spring;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	private static final Logger LOGGER = LogManager.getLogger(HomeController.class);
 	
 	// 클라이언트가 요청한 서비스 주소와 메소드를 연결
 	// 요청 메소드
@@ -18,6 +21,7 @@ public class HomeController {
 	@RequestMapping(value= {"","/"},method=RequestMethod.GET)
 	public String home() {
 		// /WEB-INF/views/home.jsp
+		LOGGER.info("게시판 프로그램 시작");
 		return "home";
 	}
 	
